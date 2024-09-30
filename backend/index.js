a7ea7b1587d37a285cfd910a4884bd9d80b6c159
Gurpreet Singh <gurpreetr1234@gmail.com>
2024-09-30 17:29:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const socketIO = require("socket.io"); // Import socket.io
 const cors = require("cors");
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 8000;
-const gameController = require("./controllers/gameController"); // Import game logic
+const gameController = require("./Controllers/GameController"); // Import game logic
 
 // Connect to the database
 connectDB();
