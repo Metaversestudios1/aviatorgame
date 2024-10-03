c5b25691d237352a90c05d4ccf603ee2ef48d2fa
nikita <nikita@gmail.com>
2024-10-03 10:49:51 +0530

    index file changes

--- backend/index.js
+++ backend/index.js
@@ -26,13 +26,13 @@ app.use(cors(corsOptions));
 app.use(express.json());
 const io = socketIO(server, {
     cors: {
-      origin: "http://localhost:3000", // Frontend URL
+      origin: "https://aviatorgame-frontend.vercel.app", // Frontend URL
       methods: ["GET", "POST"],
       credentials: true, // Allow credentials
     },
   });
 // Pass io instance to gameController
- gameController(io); // Call your game controller and pass the io instance
+ //gameController(io); // Call your game controller and pass the io instance
 
 // Define routes
 const AdminRoute = require("./Routes/AdminRoute");
