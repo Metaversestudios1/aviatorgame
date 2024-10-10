1c2c41be77e85340b995e77f5b0f0cc50beb8cc7
Gurpreet Singh <gurpreetr1234@gmail.com>
2024-10-10 12:59:12 +0530

    update game

--- frontend/src/components/setting/aviator/AviatorSetting.js
+++ frontend/src/components/setting/aviator/AviatorSetting.js
@@ -296,7 +296,7 @@ const AviatorSetting = () => {
                 placeholder="Enter End Game Range Timer"
               />
             </div>
-            <div className="my-4 relative">
+           {/* <div className="my-4 relative">
               <label
                 htmlFor="gameStartTime"
                 className="block mb-2 text-lg font-medium text-gray-900 dark:text-black"
@@ -380,7 +380,7 @@ const AviatorSetting = () => {
                 className="bg-gray-200 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-black block w-full p-2.5 "
                 placeholder="Enter Level3 Commision"
               />
-            </div>
+            </div>*/}
             <button
               type="submit"
               className="text-white bg-[#0472ff] hover:bg-blue-800 font-medium rounded-lg px-5 py-2.5"
